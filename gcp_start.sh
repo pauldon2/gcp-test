c381f322513ba79b3e4a13f5f4598c8db134d4bf
Paul <paulshevtsov@gmail.com>
2019-03-03 16:27:43 +0200

    Update gcp_start.sh

--- gcp_start.sh
+++ gcp_start.sh
@@ -2,5 +2,5 @@
 apt-get update
 apt-get install -fy python-setuptools build-essential python-dev python-pip
 pip install flask psutil
-wget https://gist.githubusercontent.com/serjs/d217e34b807e2112c6ad393304f079eb/raw/dd42e769910e753e97bed89d373f1c7491c6d95e/gistfile1.txt -O app.py
+wget https://raw.githubusercontent.com/pauldon2/gcp-test/master/start_test_serv.py -O app.py
 FLASK_APP=app.py flask run --host=0.0.0.0 --port=80
